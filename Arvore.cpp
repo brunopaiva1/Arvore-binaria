7a8f512ca8916a5df3f6e41b22fb47614e4edbb0
brunopaiva1 <brunopaivamaster10@gmail.com>
2023-03-29 16:27:29 -0300

    adicionando meses

--- Arvore.cpp
+++ Arvore.cpp
@@ -11,3 +11,21 @@ struct no{
     struct no* esq;
     struct no* dir;
 };
+
+int main(){
+    struct mes Mes[12];
+    Mes[0].nome = (char*) "Janeiro";
+    Mes[0].num = 1;
+    Mes[1].nome = (char*) "Fevereiro";
+    Mes[0].nome = (char*) "Janeiro";
+    Mes[0].nome = (char*) "Janeiro";
+    Mes[0].nome = (char*) "Janeiro";
+    Mes[0].nome = (char*) "Janeiro";
+    Mes[0].nome = (char*) "Janeiro";
+    Mes[0].nome = (char*) "Janeiro";
+    Mes[0].nome = (char*) "Janeiro";
+    Mes[0].nome = (char*) "Janeiro";
+    Mes[0].nome = (char*) "Janeiro";
+    Mes[0].nome = (char*) "Janeiro";
+
+}
