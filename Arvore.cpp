466d29fb597d9c686300175fa541d8a44f6a3641
brunopaiva1 <brunopaivamaster10@gmail.com>
2023-03-29 16:34:33 -0300

    Adicionando indices

--- Arvore.cpp
+++ Arvore.cpp
@@ -17,15 +17,25 @@ int main(){
     Mes[0].nome = (char*) "Janeiro";
     Mes[0].num = 1;
     Mes[1].nome = (char*) "Fevereiro";
-    Mes[0].nome = (char*) "Janeiro";
-    Mes[0].nome = (char*) "Janeiro";
-    Mes[0].nome = (char*) "Janeiro";
-    Mes[0].nome = (char*) "Janeiro";
-    Mes[0].nome = (char*) "Janeiro";
-    Mes[0].nome = (char*) "Janeiro";
-    Mes[0].nome = (char*) "Janeiro";
-    Mes[0].nome = (char*) "Janeiro";
-    Mes[0].nome = (char*) "Janeiro";
-    Mes[0].nome = (char*) "Janeiro";
-
+    Mes[1].num = 2;
+    Mes[2].nome = (char*) "Março";
+    Mes[2].num = 3;
+    Mes[3].nome = (char*) "Abril";
+    Mes[3].num = 4;
+    Mes[4].nome = (char*) "Maio";
+    Mes[4].num = 5;
+    Mes[5].nome = (char*) "Setembro";
+    Mes[5].num = 9;
+    Mes[6].nome = (char*) "Agosto";
+    Mes[6].num = 8;
+    Mes[7].nome = (char*) "Junho";
+    Mes[7].num = 6;
+    Mes[8].nome = (char*) "Outubro";
+    Mes[8].num = 10;
+    Mes[9].nome = (char*) "Dezembro";
+    Mes[9].num = 12;
+    Mes[10].nome = (char*) "Julho";
+    Mes[10].num = 7;
+    Mes[11].nome = (char*) "Novembro";
+    Mes[11].num = 11;
 }
